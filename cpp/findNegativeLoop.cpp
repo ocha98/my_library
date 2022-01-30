9cc0a2b4a8d9360e7f0f36fb71165f247e3124e1
shinnshinn_desk <48848793+ocha98@users.noreply.github.com>
2022-01-30 19:43:17 +0900

    [NEW] Find Negative Loop c++

---
+++ cpp/findNegativeLoop.cpp
@@ -0,0 +1,20 @@
+struct Edge{
+    int from, to, cost;
+    Edge(){}
+    Edge(int from , int to, int cost):from(from), to(to), cost(cost){}
+};
+
+bool find_negative_loop(int V, vector<Edge> es){
+    int n = es.size();
+    vector<int> dist(n);
+    for(int i = 0;i < V; ++i){
+        for(int j = 0;j < n; ++j){
+            Edge e = es[j];
+            if(dist[e.to] > dist[e.from] + e.cost){
+                dist[e.to] = dist[e.from] + e.cost;
+                if(i == V-1)return true;
+            }
+        }
+    }
+    return false;
+}
