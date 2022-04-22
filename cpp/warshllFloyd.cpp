e93b725a2525fdce9d195266714256cebcbbda42
shinnshinn_desk <48848793+ocha98@users.noreply.github.com>
2022-04-22 16:02:40 +0900

    fix bug

--- cpp/warshllFloyd.cpp
+++ cpp/warshllFloyd.cpp
@@ -1,8 +1,8 @@
 void warshall_floyd(vector<vector<int>>& cost){
     int n = cost.size();
-    for(int i = 0;i < n; ++i){
-        for(int j = 0;j < n; ++j){
-            for(int k = 0;k < n; ++k){
+    for(int k = 0;k < n; ++k){
+        for(int i = 0;i < n; ++i){
+            for(int j = 0;j < n; ++j){
                 cost[i][j] = min(cost[i][j], cost[i][k] + cost[k][j]);
             }
         }
