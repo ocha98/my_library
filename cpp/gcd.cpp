d49035bcceb3fe1907c39d0bdb267a9b035ceaa0
shinnshinn_book <48848793+ocha98@users.noreply.github.com>
2022-12-17 11:21:32 +0900

    Optimize

--- cpp/gcd.cpp
+++ cpp/gcd.cpp
@@ -1,4 +1,14 @@
-int gcd(int x, int y){
-    if(y == 0)return x;
-    return gcd(y, x%y);
+template<typename T>
+T gcd(T a, T b){
+    if(a == 0)return b;
+    if((a&1) == 0 && (b&1) == 0){
+        return 2 * gcd(a>>1, b>>1);
+    }
+    if((a&1) == 0){
+        return gcd(a>>1, b);
+    }
+    if((b&1) == 0){
+        return gcd(a, b>>1);
+    }
+    return gcd(abs(a-b)>>1, min(a, b));
 }
