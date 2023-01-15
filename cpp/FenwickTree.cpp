8f6a0a7301f6a314076aa1c8b1e147b122eee256
shinnshinn_book <48848793+ocha98@users.noreply.github.com>
2023-01-15 12:28:24 +0900

    Fix tested:http://poj.org/problem?id=1990

--- cpp/FenwickTree.cpp
+++ cpp/FenwickTree.cpp
@@ -6,15 +6,14 @@ struct FenwickTree{
     FenwickTree(int n):n(n), data(n+1){}
 
     void add(int i, T x){
-        i++;
         while(i <= n){
             data[i] += x;
             i += i & -i;
         }
     }
 
+    // sum[0, i)
     T sum(int i) const{
-        i++;
         T retu_sum = 0;
         while(i > 0){
             retu_sum += data[i];
@@ -22,5 +21,10 @@ struct FenwickTree{
         }
 
         return retu_sum;
+    }
+
+    // sum[l, r)
+    T sum(int l, int r) const {
+        return sum(r) - sum(l);
     }
 };
