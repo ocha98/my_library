7aa8d48e09ec74523cb8cbf38642f257e3a13591
shinnshinn_book <48848793+ocha98@users.noreply.github.com>
2023-01-16 11:51:14 +0900

    [Fix] tested https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_A https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_B

--- cpp/SegmentTree.cpp
+++ cpp/SegmentTree.cpp
@@ -1,40 +1,44 @@
-template<class Type, Type (*op)(Type, Type)>
-struct SegmentTree{
-    vector<Type> data;
-    Type e;
-    int N = 1;
-    SegmentTree(int n, Type e):SegmentTree(vector<Type>(n, e), e){}
+template<typename T>
+struct SegmentTree {
+    private:
+    int n = 1;
+    vector<int> node;
+    function<T(T,T)> op;
+    T e;
 
-    SegmentTree(vector<Type> v, Type e):e(e){
-        int size = v.size();
-        while(N < size)N *= 2;
-        data = vector<Type>(2*N-1, e);
-        for(int i = 0;i < size; ++i){
-            set(i, v[i]);
-        }
-    }
-
-    void set(int k, Type a){
-        k += N-1;
-        data[k] = a;
-        while(k > 0){
-            k = (k-1) / 2;
-            data[k] = op(data[2*k + 1], data[2*k + 2]);
-        }
-    }
-
-    Type query(int a, int b){// [a, b)
-        return query(a, b, 0, 0, N);
-    }
-
-    Type query(int a, int b, int k, int l, int r){
+    T query(int a, int b, int k, int l, int r) const{
         if(r <= a || b <= l)return e;
-        if(a <= l && r <= b){
-            return data[k];
-        }else{
-            Type val_l = query(a, b, 2*k + 1, l, (l+r)/2);
-            Type val_r = query(a, b, 2*k + 2, (l+r)/2, r);
-            return op(val_l, val_r);
+
+        if(a <= l && r <= b)return node[k];
+
+        T vl = query(a, b, 2*k+1, l, (l+r)/2);
+        T vr = query(a, b, 2*k+2, (l+r)/2, r);
+        return op(vl, vr);
+    }
+
+    public:
+    SegmentTree(int _n, function<T(T,T)> op, T e):op(op), e(e){
+        while(n < _n)n <<= 1;
+        node.resize(2*n-1, e);
+    }
+
+    void update(int x, T val){
+        x += n-1;
+
+        node[x] = val;
+        while(x > 0){
+            x = (x-1)/2;
+            node[x] = op(node[2*x+1], node[2*x+2]);
         }
+    }
+
+    //[a, b)
+    T query(int a, int b)const {
+        return query(a, b, 0, 0, n);
+    }
+
+    T get(int i) const{
+        i += n-1;
+        return node[i];
     }
 };
