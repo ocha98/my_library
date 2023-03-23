8faecb5200fc7625cbed98cbeaf94ffb25c0554e
MyGearBook <48848793+ocha98@users.noreply.github.com>
2023-03-23 22:57:43 +0900

    [Fix]

--- cpp/Eratosthenes.cpp
+++ cpp/Eratosthenes.cpp
@@ -1,22 +1,35 @@
 struct Eratosthenes{
+    private:
     vector<int> min_factor;
+    vector<int> primes;
+    const int MAX;
 
-    Eratosthenes(int n):min_factor(n+1) {
+    public:
+    Eratosthenes(int n):min_factor(n+1), MAX(n) {
         min_factor[1] = 1;
         for(int i = 2;i <= n; ++i){
             if(min_factor[i] != 0)continue;
+            primes.push_back(i);
             for(int j = i;j <= n;j += i){
                 min_factor[j] = i;
             }
         }
     }
 
+    vector<int> get_primes() const {
+        return primes;
+    }
+
     bool is_prime(int n){
+        assert(0 < n);
+        assert(n <= MAX);
         if(n == 1 || n == 0)return false;
         return n == min_factor[n];
     }
 
     vector<pair<int,int>> factorize(int n){
+        assert(0 < n);
+        assert(n <= MAX);
         vector<pair<int,int>> resu;
         while(n > 1){
             int p = min_factor[n];
@@ -31,9 +44,11 @@ struct Eratosthenes{
     }
 
     vector<int> divisors(int n){
+        assert(0 < n);
+        assert(n <= MAX);
         vector<int> resu = {1};
         vector<pair<int,int>> factors = factorize(n);
-        for(pair<int,int> pe : factors){
+        for(pair<int,int>& pe : factors){
             int p = pe.first;
             int e = pe.second;
 
