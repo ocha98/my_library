fb10e582585b568ef849035b3aa98ea42e4c1cf3
MyGearBook <48848793+ocha98@users.noreply.github.com>
2023-04-07 09:57:30 +0900

    [Optimize] test: https://judge.yosupo.jp/problem/scc https://onlinejudge.u-aizu.ac.jp/problems/GRL_3_C

--- cpp/scc.cpp
+++ cpp/scc.cpp
@@ -1,43 +1,40 @@
-vector<int> scc(const vector<vector<int>>& to) {
-    int n = to.size();
-    vector<int> ordv;
-    vector<bool> visited(n);
-    function<void(int)> dfs = [&](int depth){
-        visited[depth] = true;
-        for(int nxt: to[depth]){
-            if(visited[nxt])continue;
-            dfs(nxt);
+vector<int> scc(const vector<vector<int>>& to){
+    const int n = to.size();
+    vector<int> ord(n, -1), low(n);
+    vector<int> group(n, -1);
+    int group_num = 0;
+    int now_ord = 0;
+    vector<int> st;
+    st.reserve(n);
+    auto dfs = [&](auto& self, int v)->void{
+        low[v] = ord[v] = now_ord;
+        ++now_ord;
+        st.push_back(v);
+
+        for(int nxt: to[v]){
+            if(ord[nxt] == -1){
+                self(self, nxt);
+                low[v] = min(low[v], low[nxt]);
+            }else if(group[nxt] == -1){
+                low[v] = min(low[v], ord[nxt]);
+            }
         }
-        ordv.push_back(depth);
-    };
 
-    for(int i = 0;i < n; ++i){
-        if(visited[i])continue;
-        dfs(i);
-    }
-
-    vector<vector<int>> inv_to(n);
-    for(int i = 0;i < n; ++i){
-        for(int v : to[i]){
-            inv_to[v].push_back(i);
-        }
-    }
-
-    vector<int> group(n);
-    int k = 1;
-    function<void(int)> rdfs = [&](int depth){
-        group[depth] = k;
-        for(int nxt : inv_to[depth]){
-            if(group[nxt])continue;
-            rdfs(nxt);
+        if(ord[v] == low[v]){
+            while(true){
+                int u = st.back();
+                st.pop_back();
+                group[u] = group_num;
+                if(v == u)break;
+            }
+            ++group_num;
         }
     };
 
-    reverse(ordv.begin(), ordv.end());
-    for(int v : ordv){
-        if(group[v])continue;
-        rdfs(v);
-        ++k;
+    for(int i = 0;i < n; ++i){
+        if(ord[i] == -1){
+            dfs(dfs, i);
+        }
     }
 
     return group;
