29387d7c9dd9330041cef63b16bee6e07cd50e53
MyGearBook <48848793+ocha98@users.noreply.github.com>
2023-05-12 17:17:20 +0900

    [Fix]

--- cpp/Eratosthenes.cpp
+++ cpp/Eratosthenes.cpp
@@ -1,17 +1,31 @@
 struct Eratosthenes{
     private:
     vector<int> min_factor;
+    vector<int> prev_divisor;
     vector<int> primes;
+    vector<int> min_factor_e;
     const int MAX;
 
     public:
-    Eratosthenes(int n):min_factor(n+1), MAX(n) {
+    Eratosthenes(int n):min_factor(n+1), min_factor_e(n+1), prev_divisor(n+1), MAX(n) {
         min_factor[1] = 1;
         for(int i = 2;i <= n; ++i){
-            if(min_factor[i] != 0)continue;
-            primes.push_back(i);
-            for(int j = i;j <= n;j += i){
-                min_factor[j] = i;
+            if(min_factor[i] == 0){
+                min_factor[i] = i;
+                primes.push_back(i);
+                min_factor_e[i] = 1;
+            }
+
+            for(int p: primes){
+                if(p*i > n || p > min_factor[i])break;
+                min_factor[p*i] = p;
+                if(p == min_factor[i]){
+                    min_factor_e[p*i] = min_factor_e[i]+1;
+                    prev_divisor[p*i] = prev_divisor[i];
+                }else{
+                    min_factor_e[p*i] = 1;
+                    prev_divisor[p*i] = i;
+                }
             }
         }
     }
@@ -20,39 +34,34 @@ struct Eratosthenes{
         return primes;
     }
 
-    bool is_prime(int n){
-        assert(0 < n);
-        assert(n <= MAX);
+    bool is_prime(int n) const {
+        assert(0 < n && n <= MAX);
         if(n == 1 || n == 0)return false;
         return n == min_factor[n];
     }
 
-    vector<pair<int,int>> factorize(int n){
-        assert(0 < n);
-        assert(n <= MAX);
+    vector<pair<int,int>> factorize(int n) const {
+        assert(1 <= n && n <= MAX);
         vector<pair<int,int>> resu;
         while(n > 1){
-            int p = min_factor[n];
-            int e = 0;
-            while(min_factor[n] == p){
-                n /= p;
-                e++;
-            }
-            resu.push_back(make_pair(p, e));
+            const int p = min_factor[n];
+            const int e = min_factor_e[n];
+            resu.emplace_back(make_pair(p, e));
+            n = prev_divisor[n];
         }
         return resu;
     }
 
-    vector<int> divisors(int n){
-        assert(0 < n);
-        assert(n <= MAX);
+    vector<int> divisors(int n) const {
+        assert(1 < n && n <= MAX);
+
         vector<int> resu = {1};
         vector<pair<int,int>> factors = factorize(n);
-        for(pair<int,int>& pe : factors){
-            int p = pe.first;
-            int e = pe.second;
+        for(const pair<int,int>& pe : factors){
+            const int p = pe.first;
+            const int e = pe.second;
 
-            int s = resu.size();
+            const int s = resu.size();
             for(int i = 0;i < s; ++i){
                 int v = 1;
                 for(int ii = 0;ii < e; ++ii){
