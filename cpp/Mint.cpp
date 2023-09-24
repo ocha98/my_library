d9bc43eac8568375e77b3e2ea470b7c2f64937a6
MyGearBook <48848793+ocha98@users.noreply.github.com>
2023-09-24 11:55:49 +0900

    C++20: constexpr

--- cpp/Mint.cpp
+++ cpp/Mint.cpp
@@ -1,32 +1,22 @@
+template<long long MOD>
 struct Mint{
     private:
     long long val;
 
     public:
-    Mint(long long x){
+    constexpr Mint():val(0){}
+    constexpr Mint(long long x){
         x %= MOD;
         if(x < 0)x += MOD;
         val = x;
     }
-    Mint():val(0){}
 
-    Mint operator*(const Mint& a)const {
-        return Mint(*this) *= a;
-    }
+    constexpr Mint operator*(const Mint& a)const { return Mint(*this) *= a; }
+    constexpr Mint operator/(const Mint& a)const { return Mint(*this) /= a; }
+    constexpr Mint operator+(const Mint& a)const { return Mint(*this) += a; }
+    constexpr Mint operator-(const Mint& a)const { return Mint(*this) -= a; }
 
-    Mint operator/(const Mint& a)const {
-        return Mint(*this) /= a;
-    }
-
-    Mint operator+(const Mint& a)const {
-        return Mint(*this) += a;
-    }
-
-    Mint operator-(const Mint& a)const {
-        return Mint(*this) -= a;
-    }
-
-    Mint operator-()const {
+    constexpr Mint operator-()const {
         Mint x(*this);
         if(x.val != 0){
             x.val = MOD - x.val;
@@ -34,56 +24,52 @@ struct Mint{
         return x;
     }
 
-    Mint& operator++(){
+    constexpr Mint& operator++(){
         *this += 1;
         return *this;
     }
 
-    Mint operator++(int){
+    constexpr Mint operator++(int){
         Mint x(*this);
         ++(*this);
         return x;
     }
 
-    Mint& operator--(){
+    constexpr Mint& operator--(){
         *this -= 1;
         return *this;
     }
 
-    Mint operator--(int){
+    constexpr Mint operator--(int){
         Mint x(*this);
         --(*this);
         return x;
     }
 
-    Mint& operator+= (const Mint &a){
+    constexpr Mint& operator+= (const Mint &a){
         this->val += a.val;
         if(this->val >= MOD)this->val -= MOD;
         return *this;
     }
 
-    Mint& operator-= (const Mint &a){
+    constexpr Mint& operator-= (const Mint &a){
         this->val +=  MOD - a.val;
         if(this->val >= MOD)this->val -= MOD;
         return *this;
     }
 
-    Mint& operator*= (const Mint &a){
+    constexpr Mint& operator*= (const Mint &a){
         this->val *= a.val;
         this->val %= MOD;
         return *this;
     }
 
-    Mint& operator/= (const Mint & a){
+    constexpr Mint& operator/= (const Mint & a){
         *this *= a.inv();
         return *this;
     }
 
-    Mint inv()const{
-        return pow(MOD - 2);
-    }
-
-    Mint pow(long long n)const {
+    constexpr Mint pow(long long n)const {
         if(n == 0)return 1;
         Mint retu(1);
         Mint x(*this);
@@ -97,18 +83,22 @@ struct Mint{
         return retu;
     }
 
-    long long get() const{
-        return this->val;
-    }
+    constexpr Mint inv()const{ return pow(MOD - 2); }
+    constexpr long long get() const { return this->val; }
+    constexpr long long mod() const { return MOD; }
 };
 
-istream& operator>>(istream& is, Mint& a){
+template<long MOD>
+istream& operator>>(istream& is, Mint<MOD>& a){
     long long val;
     is >> val;
     a = val;
     return is;
 }
 
-ostream& operator<<(ostream& os, const Mint& a){
+template<long MOD>
+ostream& operator<<(ostream& os, const Mint<MOD>& a){
     return os << a.get();
 }
+
+using mint = Mint<998244353>;
