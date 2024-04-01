e5d959a554c18666f55564ff34250b168c66460f
ocha98 <48848793+ocha98@users.noreply.github.com>
2024-04-01 14:18:17 +0900

    [Fix] 2d segtree

--- rust/SegmentTree2D.rs
+++ rust/SegmentTree2D.rs
@@ -29,24 +29,24 @@ impl<T: Monoid> SegTree2D<T> {
 
         self.data[i][j] = val;
 
+        let mut w = j>>1;
+        while w > 0 {
+            self.data[i][w] = self.data[i][w << 1].op(&self.data[i][w << 1 | 1]);
+            w >>= 1;
+        }
+
         let mut h = i>>1;
         while h > 0 {
             self.data[h][j] = self.data[h << 1][j].op(&self.data[h<<1 | 1][j]);
+            let mut w = j>>1;
+            while w > 0 {
+                self.data[h][w] = self.data[h][w << 1].op(&self.data[h][w << 1 | 1]);
+                w >>= 1;
+            }
             h >>= 1;
         }
-
-        let mut w = j>>1;
-        while w > 0 {
-            self.data[i][w] = self.data[i][w<<1].op(&self.data[i][w<<1 | 1]);
-            let mut h = i>>1;
-            while h > 0 {
-                self.data[h][w] = self.data[h<<1][w].op(&self.data[h<<1 | 1][w]);
-                h >>= 1;
-            }
-            w >>= 1;
-        }
     }
-    
+
     fn query<R>(&self, rng: R) -> T
     where R: std::ops::RangeBounds<(usize, usize)>
     {
@@ -69,48 +69,48 @@ impl<T: Monoid> SegTree2D<T> {
         ri += self.h;
         rj += self.w;
 
-        let mut lw = lj;
-        let mut rw = rj;
-        while lw < rw {
-            if lw&1 == 1 {
-                let mut lh = li;
-                let mut rh = ri;
-                while lh < rh {
-                    if lh&1 == 1 {
+        let mut lh = li;
+        let mut rh = ri;
+        while lh < rh {
+            if lh&1 == 1 {
+                let mut lw = lj;
+                let mut rw = rj;
+                while lw < rw {
+                    if lw&1 == 1 {
                         ans = ans.op(&self.data[lh][lw]);
-                        lh += 1;
+                        lw += 1;
                     }
-                    if rh &1 == 1 {
-                        rh -= 1;
-                        ans = ans.op(&self.data[rh][lw]);
+                    if rw&1 == 1 {
+                        rw -= 1;
+                        ans = ans.op(&self.data[lh][rw]);
                     }
 
-                    lh >>= 1;
-                    rh >>= 1;
+                    lw >>= 1;
+                    rw >>= 1;
                 }
-                lw += 1;
+                lh += 1;
             }
-            if rw&1 == 1 {
-                rw -= 1;
-                let mut lh = li;
-                let mut rh = ri;
-                while lh < rh {
-                    if lh&1 == 1 {
-                        ans = ans.op(&self.data[lh][rw]);
-                        lh += 1;
+            if rh&1 == 1 {
+                rh -= 1;
+                let mut lw = lj;
+                let mut rw = rj;
+                while lw < rw {
+                    if lw&1 == 1 {
+                        ans = ans.op(&self.data[rh][lw]);
+                        lw += 1;
                     }
-                    if rh &1 == 1 {
-                        rh -= 1;
+                    if rw&1 == 1 {
+                        rw -= 1;
                         ans = ans.op(&self.data[rh][rw]);
                     }
 
-                    lh >>= 1;
-                    rh >>= 1;
+                    lw >>= 1;
+                    rw >>= 1;
                 }
             }
 
-            lw >>= 1;
-            rw >>= 1;
+            lh >>= 1;
+            rh >>= 1;
         }
 
         ans
